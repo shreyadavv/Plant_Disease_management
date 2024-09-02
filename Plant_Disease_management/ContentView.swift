95b1d3020004aa1991ebdeb301ee0df403c295ea
Shreya Yadav <shreyadav1527@gmail.com>
2024-09-02 23:05:11 +0530

    Initial Commit

---
+++ Plant_Disease_management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plant_Disease_management
+//
+//  Created by Shreya Yadav on 02/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plant_Disease_management/Plant_Disease_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_Disease_managementApp.swift
+//  Plant_Disease_management
+//
+//  Created by Shreya Yadav on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Plant_Disease_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
